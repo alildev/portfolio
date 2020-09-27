0952666a42b858aa2839546b484b678c0ab6ce67
waysideoff <alillard0050@gmail.com>
2020-09-27 14:53:12 -0400

    menuItems open toggle added

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.menu-items');
 
 //MAIN TOGGLE
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('open');
+    menuItems.classList.toggle('open');
 })
